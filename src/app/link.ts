1c03dbb41e0a6aec9a4d2c78f3c4847520c8adb9
mwangi micha <michameiu@gmail.com>
2017-01-18 16:34:34 +0300

    changed link

--- src/app/link.ts
+++ src/app/link.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core'
 
 @Injectable()
 export class Link{
-     uri:string="http://localhost:8000/"
+     //uri:string="http://localhost:8000/"
     client_id:string="dnFhSdWfy2XjFqTzpSLMbYqRKOgGei2eG7hUnNDS"
-    //uri:string="http://uoosc.cloudapp.net/"
+    uri:string="http://uoosc.cloudapp.net/"
 }
