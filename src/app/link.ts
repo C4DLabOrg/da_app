240d748109e22e8a367158f2dd1a48c62e5f9cc2
Dennis Chivatsi <dennischivatsi@gmail.com>
2017-11-14 13:35:21 +0300

    Merge Issues

--- src/app/link.ts
+++ src/app/link.ts
@@ -5,10 +5,10 @@ export class Link {
   // uri:string="http://api.oosc/"
   client_id: string = "dnFhSdWfy2XjFqTzpSLMbYqRKOgGei2eG7hUnNDS"
   // uri:string="http://uoosc.cloudapp.net/"
-  //uri:string="http://oosc.cloudapp.net/"
+  uri:string="http://oosc.cloudapp.net/"
   // uri: string = "http://localhost:8000/"
   //uri:string="http://app.attendance.co.ke/"
-  uri: string = "https://api.attendance.co.ke/"
+  //uri: string = "https://api.attendance.co.ke/"
 
 }
 
